facb16b7e8be639c931b1ecdcc2ed3630dfcceb2
lewinskig <lewinskig@gmail.com>
2026-03-21 07:48:33 +0100

    initial from start.spring.io

---
+++ src/main/kotlin/com/lewinskig/geofences/GeofencesApplication.kt
@@ -0,0 +1,11 @@
+package com.lewinskig.geofences
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GeofencesApplication
+
+fun main(args: Array<String>) {
+	runApplication<GeofencesApplication>(*args)
+}
---
+++ src/test/kotlin/com/lewinskig/geofences/GeofencesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.lewinskig.geofences
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GeofencesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
