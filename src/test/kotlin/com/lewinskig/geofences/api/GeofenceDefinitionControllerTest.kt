a93cab534c6c644d35f0203d0a28457d2fb8d9d0
Grzegorz Lewinski <g.lewinski@ocado.com>
2026-03-23 08:32:28 +0100

    add Transactional to GeofenceDefinitionControllerTest not to leave the data after test

--- src/test/kotlin/com/lewinskig/geofences/api/GeofenceDefinitionControllerTest.kt
+++ src/test/kotlin/com/lewinskig/geofences/api/GeofenceDefinitionControllerTest.kt
@@ -7,9 +7,11 @@ import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.post
+import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class GeofenceDefinitionControllerTest {
 
     @Autowired
